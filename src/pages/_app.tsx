d95363fa516b2315f3c33140522aceafa0141b19
MelkdeSousa <melk.sousap@gmail.com>
2021-04-20 17:44:00 -0300

    ✨ feat: lesson 2

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import '../styles/global.scss'
+
+import { AppProps } from 'next/app'
+
+import Header from '../components/Header'
+import Player from '../components/Player'
+
+import styles from '../styles/app.module.scss'
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <div className={styles.wrapper}>
+    <main>
+      <Header />
+      <Component {...pageProps} />
+    </main>
+
+    <Player />
+  </div>
+)
+
+export default MyApp
